7127bbbd4aa9c3aafa1782bf304c8d120b13b9d3
YunYouJun <me@yunyoujun.cn>
2020-02-12 15:05:24 +0800

    :wrench: vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,68 @@
 module.exports = {
-  title: 'Saying',
-  description: 'oracle'
-}
+  title: "Say",
+  description: "oracle",
+  head: [
+    ["link", { rel: "icon", href: "/logo.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#6200ee" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "#6200ee" }
+    ],
+    ["link", { rel: "apple-touch-icon", href: "/logo.png" }],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/logo.png",
+        color: "#6200ee"
+      }
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/logo.png"
+      }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#6200ee" }]
+  ],
+  themeConfig: {
+    logo: "/logo.png",
+    nav: [
+      { text: "指南", link: "/guide/" },
+      {
+        text: "句子",
+        link: "/sentence/"
+      },
+      {
+        text: "词语",
+        link: "/word/"
+      }
+    ],
+    repo: "ElpsyCN/say",
+    docsDir: "docs",
+    editLinks: true,
+    editLinkText: "来增加更多有趣的东西吧！",
+    smoothScroll: true,
+    sidebar: "auto"
+  },
+  plugins: [
+    "@vuepress/back-to-top",
+    "@vuepress/medium-zoom",
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-121354150-12"
+      }
+    ],
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ]
+  ]
+};
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,14 @@
 ---
 home: true
-heroImage: /hero.png
+heroImage: /logo.png
 actionText: El Psy Congroo
-actionLink: /
+actionLink: /guide
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: Copyright © 2018-present YunYouJun
+  - title: 中二至上
+    details: 今日は……风が騒がしいな…（今日的风儿甚是喧嚣……）
+  - title: 兴趣驱动
+    details: 我只是一个兴趣使然的英雄罢了。
+  - title: 高羞耻度
+    details: 人生在世，别太在意别人的看法，因为你无法取悦所有人。
+footer: Copyright © 2018-2020 YunYouJun
 ---
-
-# 疾风
